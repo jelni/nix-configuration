7c1bbab5c60eb464337c8de6b4dcd8ee7a9782ba
jel <25802745+jelni@users.noreply.github.com>
2025-10-15 22:33:57 +0200

    configure htop

--- home.nix
+++ home.nix
@@ -1,17 +1,33 @@
-{ pkgs, ... }:
+{ config, ... }:
 {
   home = {
     username = "jel";
     homeDirectory = "/home/jel";
-
-    packages = with pkgs; [
-      htop
-    ];
-
     stateVersion = "25.05";
   };
 
   programs = {
+    htop = {
+      enable = true;
+
+      settings = {
+        tree_view = 1;
+        header_layout = "one_100";
+      }
+      // (
+        with config.lib.htop;
+        leftMeters [
+          (bar "AllCPUs4")
+          (bar "Memory")
+          (text "ZFSARC")
+          (text "DiskIO")
+          (text "Uptime")
+          (text "LoadAverage")
+          (text "Tasks")
+        ]
+      );
+    };
+
     ssh = {
       enable = true;
       extraConfig = ''
