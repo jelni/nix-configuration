972650c2a1d1e4272a9a562a436c33e4cd7ed817
jel <25802745+jelni@users.noreply.github.com>
2025-11-13 23:27:09 +0100

    enable hashed MAC addresses

--- configuration.nix
+++ configuration.nix
@@ -438,45 +438,50 @@
       ];
     };
 
-    networkmanager.ensureProfiles = {
-      environmentFiles = [ config.age.secrets.wireless-networks.path ];
+    networkmanager = {
+      wifi.macAddress = "stable-ssid";
+      ethernet.macAddress = "stable";
 
-      profiles =
-        let
-          profile = name: {
-            connection = {
-              id = "\$${name}_SSID";
-              type = "wifi";
+      ensureProfiles = {
+        environmentFiles = [ config.age.secrets.wireless-networks.path ];
+
+        profiles =
+          let
+            profile = name: {
+              connection = {
+                id = "\$${name}_SSID";
+                type = "wifi";
+              };
+
+              wifi.ssid = "\$${name}_SSID";
+
+              wifi-security = {
+                key-mgmt = "\$${name}_SECURITY";
+                psk = "\$${name}_PASS";
+              };
             };
-
-            wifi.ssid = "\$${name}_SSID";
-
-            wifi-security = {
-              key-mgmt = "\$${name}_SECURITY";
-              psk = "\$${name}_PASS";
-            };
-          };
-        in
-        builtins.listToAttrs (
-          builtins.map
-            (name: {
-              inherit name;
-              value = profile (lib.toUpper name);
-            })
-            # thank you https://t.me/totalnie_zmyslone_slowa
-            [
-              "grawideon"
-              "gidflugh"
-              "kataplaster"
-              "alfabetyzator"
-              "genetication"
-              "adoment"
-              "abstrulich"
-              "wpajpowac"
-              "siekieralt"
-              "technokokarda"
-            ]
-        );
+          in
+          builtins.listToAttrs (
+            builtins.map
+              (name: {
+                inherit name;
+                value = profile (lib.toUpper name);
+              })
+              # thank you https://t.me/totalnie_zmyslone_slowa
+              [
+                "grawideon"
+                "gidflugh"
+                "kataplaster"
+                "alfabetyzator"
+                "genetication"
+                "adoment"
+                "abstrulich"
+                "wpajpowac"
+                "siekieralt"
+                "technokokarda"
+              ]
+          );
+      };
     };
   };
 
