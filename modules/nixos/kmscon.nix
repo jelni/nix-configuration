65b8e28b44cd8e1f81d97db58a5fd77957d34a75
jel <25802745+jelni@users.noreply.github.com>
2026-02-19 23:42:45 +0100

    use XKB configuration in kmscon

--- modules/nixos/kmscon.nix
+++ modules/nixos/kmscon.nix
@@ -1,4 +1,9 @@
-{ ... }:
+{ config, ... }:
 {
-  services.kmscon.enable = true;
+  services.kmscon = {
+    enable = true;
+    useXkbConfig = true;
+  };
+
+  systemd.services."kmsconvt@".environment.XKB_CONFIG_ROOT = config.services.xserver.xkb.dir;
 }
