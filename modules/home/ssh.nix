ec27fc85e86e4941159e8b461efb84a0f92b937a
jel <25802745+jelni@users.noreply.github.com>
2026-02-19 23:42:45 +0100

    enable SSH agent forwarding

--- modules/home/ssh.nix
+++ modules/home/ssh.nix
@@ -3,6 +3,10 @@
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false; # remove once this is the default
-    matchBlocks."*".identityAgent = "~/.1password/agent.sock";
+
+    matchBlocks."*" = {
+      forwardAgent = true;
+      identityAgent = "~/.1password/agent.sock";
+    };
   };
 }
