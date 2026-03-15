0eee3cfea72d0a38f6754e845d7a501ae84f9f04
jel <25802745+jelni@users.noreply.github.com>
2026-03-15 21:47:43 +0100

    add hlebak host SSH key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,15 +1,18 @@
 let
   user-jel = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIeyqBh/je0A8dMXB0m9jxZ0nBWHc5Hj+dFmFBiT5Mdw";
+  host-hlebak = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOLi8mbXX3+fmtHj47rM0wWcwcfgivmuXeB2oSIA/1yf";
   host-hydromechanizator = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKpc44dkGAK435at+4wQbrgfv21hqXe03rQrna93QT3u";
   host-wsl = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHu2STpuDoldavVByGvUN9e+tP/BfTorGkiixYGVO/4G";
 
   baremetal = [
     user-jel
+    host-hlebak
     host-hydromechanizator
   ];
 
   userspace = [
     user-jel
+    host-hlebak
     host-hydromechanizator
   ];
 
