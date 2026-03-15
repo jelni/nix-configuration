16c769ec73f9127dcf486a56f6920808222ed235
jel <25802745+jelni@users.noreply.github.com>
2026-03-16 00:23:51 +0100

    disable Pulseaudio Always Sink

--- modules/nixos/pipewire.nix
+++ modules/nixos/pipewire.nix
@@ -2,6 +2,7 @@
 {
   services.pipewire = {
     enable = true;
+    extraConfig.pipewire-pulse.disable-always-sink."pulse.properties"."pulse.cmd.always-sink" = false;
     pulse.enable = true;
   };
 }
