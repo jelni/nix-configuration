1cedb943e1089d029c00befa3a8ad17e9a2a9b86
jel <25802745+jelni@users.noreply.github.com>
2026-03-17 00:44:35 +0100

    improve OpenSSH security

--- modules/nixos/openssh.nix
+++ modules/nixos/openssh.nix
@@ -1,4 +1,12 @@
 { ... }:
 {
-  services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+
+    settings = {
+      KbdInteractiveAuthentication = false;
+      PasswordAuthentication = false;
+      PermitRootLogin = "no";
+    };
+  };
 }
