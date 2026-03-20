74aed6213b4d081607d2877d0d4062132b82dfe5
jel <25802745+jelni@users.noreply.github.com>
2026-03-21 00:50:22 +0100

    configure qBittorrent

--- modules/nixos/qbittorrent.nix
+++ modules/nixos/qbittorrent.nix
@@ -4,6 +4,33 @@
     enable = true;
     openFirewall = true;
     profileDir = "/srv/";
+
+    serverConfig = {
+      Application.FileLogger.Enabled = false;
+
+      BitTorrent.Session = {
+        DisableAutoTMMByDefault = false;
+
+        DisableAutoTMMTriggers = {
+          CategorySavePathChanged = false;
+          DefaultSavePathChanged = false;
+        };
+
+        MaxConnections = -1;
+        MaxConnectionsPerTorrent = -1;
+        MaxUploads = -1;
+        MaxUploadsPerTorrent = -1;
+        QueueingSystemEnabled = false;
+      };
+
+      LegalNotice.Accepted = true;
+
+      Preferences = {
+        General.StatusbarExternalIPDisplayed = true;
+        WebUI.LocalHostAuth = false;
+      };
+    };
+
     torrentingPort = 25556;
   };
 }
--- modules/nixos/tailscale.nix
+++ modules/nixos/tailscale.nix
@@ -1,8 +1,15 @@
 { ... }:
 {
-  services.tailscale = {
-    enable = true;
-    extraSetFlags = [ "--ssh" ];
-    openFirewall = true;
+  services = {
+    tailscale = {
+      enable = true;
+      extraSetFlags = [ "--ssh" ];
+      openFirewall = true;
+    };
+
+    qbittorrent.serverConfig.Preferences.WebUI = {
+      AuthSubnetWhitelist = "100.64.0.0/10, fd7a:115c:a1e0::/48";
+      AuthSubnetWhitelistEnabled = true;
+    };
   };
 }
