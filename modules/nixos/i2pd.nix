84e3e1af5cbf130bbc4e6b976273a02c368aedd4
jel <25802745+jelni@users.noreply.github.com>
2026-04-17 01:58:44 +0200

    start qBittorrent after i2pd

--- modules/nixos/i2pd.nix
+++ modules/nixos/i2pd.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 {
   networking.firewall.allowedTCPPorts = [ config.services.i2pd.proto.http.port ];
 
@@ -22,5 +22,18 @@
     };
 
     qbittorrent.serverConfig.BitTorrent.Session.I2P.Enabled = true;
+  };
+
+  systemd.services.qbittorrent = {
+    after = [ "i2pd.service" ];
+    path = [ pkgs.netcat ];
+
+    preStart = ''
+      while ! nc -z ${config.services.i2pd.proto.sam.address} ${toString config.services.i2pd.proto.sam.port}; do
+        sleep 1
+      done
+    '';
+
+    requires = [ "i2pd.service" ];
   };
 }
