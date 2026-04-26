e5ded1458722a30a24db82b93f8066f81143dbcf
jel <25802745+jelni@users.noreply.github.com>
2026-04-27 00:51:01 +0200

    disable Halloy update checks

--- modules/home/halloy.nix
+++ modules/home/halloy.nix
@@ -5,16 +5,22 @@
     package = pkgs.unstable.halloy;
 
     settings = {
-      buffer.buffer_separator.hide_when_all_read = true;
-      buffer.text_input.auto_format = "all";
-
-      notifications.direct_message = {
-        sound = "peck";
-        show_toast = true;
-        show_content = true;
+      buffer = {
+        buffer_separator.hide_when_all_read = true;
+        text_input.auto_format = "all";
       };
 
-      notifications.highlight.sound = "dong";
+      check_for_update_on_launch = false;
+
+      notifications = {
+        direct_message = {
+          sound = "peck";
+          show_toast = true;
+          show_content = true;
+        };
+
+        highlight.sound = "dong";
+      };
 
       servers.soju = {
         nickname = "jel";
