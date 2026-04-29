b46b1008ab78620b783f97f0c5b7323a583cac69
jel <git@jel.gay>
2026-04-29 11:51:03 +0200

    write change IDs in Jujutsu commits, and update committer email

--- modules/home/jujutsu.nix
+++ modules/home/jujutsu.nix
@@ -4,9 +4,13 @@
     enable = true;
     package = pkgs.unstable.jujutsu;
 
-    settings.user = {
-      name = "jel";
-      email = "25802745+jelni@users.noreply.github.com";
+    settings = {
+      git.write-change-id-header = true;
+
+      user = {
+        email = "git@jel.gay";
+        name = "jel";
+      };
     };
   };
 }
