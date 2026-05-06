6e472d92e57f7a382092b6cd279746c494a46465
jel <git@jel.gay>
2026-05-06 16:45:40 +0200

    add Opus stream

--- modules/nixos/mpd.nix
+++ modules/nixos/mpd.nix
@@ -1,24 +1,35 @@
 {
   services =
     let
-      port = "6601";
+      flac-port = "6601";
+      opus-port = "6602";
     in
     {
-      caddy.virtualHosts."https://radio.hszyr.space".extraConfig = "reverse_proxy localhost:${port}";
+      caddy.virtualHosts."https://radio.hszyr.space".extraConfig = ''
+        handle / { reverse_proxy localhost:${flac-port} }
+        handle /opus { reverse_proxy localhost:${opus-port} }
+      '';
 
       mpd = {
         enable = true;
 
-        extraConfig = ''
-          audio_output {
-            type "httpd"
-            name "żyrardio"
-            port "${port}"
-            bind_to_address "0.0.0.0"
-            encoder "flac"
-            genre "żyrobeat"
-          }
-        '';
+        extraConfig =
+          let
+            output = name: port: encoder: ''
+              audio_output {
+                type "httpd"
+                name "${name}"
+                port "${port}"
+                bind_to_address "0.0.0.0"
+                encoder "${encoder}"
+                genre "żyrobeat"
+              }
+            '';
+          in
+          ''
+            ${output "żyrardio" flac-port "flac"}
+            ${output "żyrardio-opus" opus-port "opus"}
+          '';
 
         musicDirectory = "/srv/qBittorrent/downloads";
       };
