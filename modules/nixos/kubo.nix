626950c51e4dee80c26a8ed1fad7158e1b4d6bed
jel <git@jel.gay>
2026-05-23 02:37:01 +0200

    reverse proxy Kubo

--- modules/nixos/kubo.nix
+++ modules/nixos/kubo.nix
@@ -1,16 +1,18 @@
 let
-  port = 5001;
+  port = "5001";
 in
 {
-  networking.firewall.allowedTCPPorts = [ port ];
+  services = {
+    caddy.virtualHosts."https://ipfs.jel.gay".extraConfig = "reverse_proxy localhost:${port}";
 
-  services.kubo = {
-    enable = true;
-    enableGC = true;
+    kubo = {
+      enable = true;
+      enableGC = true;
 
-    settings.Addresses.Gateway = [
-      "/ip4/0.0.0.0/tcp/${toString port}"
-      "/ip6/::/tcp/${toString port}"
-    ];
+      settings.Addresses.Gateway = [
+        "/ip4/0.0.0.0/tcp/${port}"
+        "/ip6/::/tcp/${port}"
+      ];
+    };
   };
 }
