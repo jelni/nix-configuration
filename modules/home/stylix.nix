c6d4c3ba928a4e71d8e4dc2dde9e616fface7ad9
jel <git@jel.gay>
2026-06-04 00:56:42 +0200

    lock Stylix to 26.05

--- modules/home/stylix.nix
+++ modules/home/stylix.nix
@@ -1,13 +1,11 @@
 {
-  gtk.gtk4.theme = null;
-
   stylix.targets = {
     bat.enable = false;
     firefox.profileNames = [ "default" ];
     fish.enable = false;
     qt.platform = "qtct";
     vesktop.enable = false;
-    vscode.enable = false;
+    vscodium.enable = false;
     zed.enable = false;
   };
 }
