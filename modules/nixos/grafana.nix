f0ae14b2cf1e19513ea11f56bf0e1e53f6226a44
jel <git@jel.gay>
2026-06-08 21:46:12 +0200

    set Grafana domain and disable Grafana Cloud, help and news

--- modules/nixos/grafana.nix
+++ modules/nixos/grafana.nix
@@ -6,23 +6,34 @@
     owner = "grafana";
   };
 
-  services = {
-    caddy.virtualHosts."https://grafana.jel.gay".extraConfig =
-      "reverse_proxy ${config.services.grafana.settings.server.http_addr}:${toString config.services.grafana.settings.server.http_port}";
+  services =
+    let
+      domain = "grafana.jel.gay";
+    in
+    {
+      caddy.virtualHosts."https://${domain}".extraConfig =
+        "reverse_proxy ${config.services.grafana.settings.server.http_addr}:${toString config.services.grafana.settings.server.http_port}";
 
-    grafana = {
-      enable = true;
-      provision.enable = true;
+      grafana = {
+        enable = true;
+        provision.enable = true;
 
-      settings = {
-        "auth.anonymous" = {
-          enabled = true;
-          org_name = "Public";
+        settings = {
+          "auth.anonymous" = {
+            enabled = true;
+            org_name = "Public";
+          };
+
+          cloud_migration.enabled = false;
+          help.enabled = false;
+          news.news_feed_enabled = false;
+          security.secret_key = "$__file{${config.age.secrets.grafana-secret-key.path}}";
+
+          server = {
+            inherit domain;
+            http_port = 3030;
+          };
         };
-
-        security.secret_key = "$__file{${config.age.secrets.grafana-secret-key.path}}";
-        server.http_port = 3030;
       };
     };
-  };
 }
