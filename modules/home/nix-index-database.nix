f2dc849dbc3aaf822163f54b553a9cb12adb043b
jel <git@jel.gay>
2026-06-11 12:47:24 +0200

    disable nix-index shell integration

--- modules/home/nix-index-database.nix
+++ modules/home/nix-index-database.nix
@@ -1,5 +1,9 @@
 { inputs, ... }:
 {
   imports = [ inputs.nix-index-database.homeModules.default ];
-  programs.nix-index-database.comma.enable = true;
+
+  programs = {
+    nix-index.enable = false;
+    nix-index-database.comma.enable = true;
+  };
 }
