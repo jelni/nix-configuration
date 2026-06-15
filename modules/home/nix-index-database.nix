2b89b717cc4d1a25bf0f1aaa44ce350424ecda2c
jel <git@jel.gay>
2026-06-15 22:53:20 +0200

    re-enable nix-index, but disable the fish integration

--- modules/home/nix-index-database.nix
+++ modules/home/nix-index-database.nix
@@ -3,7 +3,7 @@
   imports = [ inputs.nix-index-database.homeModules.default ];
 
   programs = {
-    nix-index.enable = false;
+    nix-index.enableFishIntegration = false;
     nix-index-database.comma.enable = true;
   };
 }
