312706d34142d6fe9524b5abfa1fa1b4649a61da
jel <git@jel.gay>
2026-07-05 10:46:26 +0200

    enable Prometheus node_exporter and postgres_exporter

--- modules/nixos/postgresql.nix
+++ modules/nixos/postgresql.nix
@@ -1,6 +1,24 @@
-{
-  services.postgresql = {
-    enable = true;
-    extensions = ps: with ps; [ pgvector ];
+{ config, ... }: {
+  services = {
+    postgresql = {
+      enable = true;
+      extensions = ps: with ps; [ pgvector ];
+    };
+
+    prometheus = {
+      exporters.postgres.enable = config.services.prometheus.enable;
+
+      scrapeConfigs = [
+        {
+          job_name = "postgres";
+
+          static_configs =
+            let
+              postgres = config.services.prometheus.exporters.postgres;
+            in
+            [ { targets = [ "${postgres.listenAddress}:${toString postgres.port}" ]; } ];
+        }
+      ];
+    };
   };
 }
--- modules/nixos/prometheus.nix
+++ modules/nixos/prometheus.nix
@@ -1,5 +1,4 @@
-{ config, ... }:
-{
+{ config, ... }: {
   services = {
     caddy.globalConfig = "metrics { per_host }";
 
@@ -11,6 +10,25 @@
       }
     ];
 
-    prometheus.enable = true;
+    prometheus = {
+      enable = true;
+
+      exporters.node = {
+        enable = true;
+        enabledCollectors = [ "systemd" ];
+      };
+
+      scrapeConfigs = [
+        {
+          job_name = "node";
+
+          static_configs =
+            let
+              node = config.services.prometheus.exporters.node;
+            in
+            [ { targets = [ "${node.listenAddress}:${toString node.port}" ]; } ];
+        }
+      ];
+    };
   };
 }
