3ca2df48391024c100360dedc23c1ee38d33fd76
jel <git@jel.gay>
2026-07-24 22:31:34 +0200

    remove Status Icons, and add AppIndicator and KStatusNotifierItem Support GNOME extension

--- modules/home/gnome.nix
+++ modules/home/gnome.nix
@@ -6,12 +6,12 @@
     extensions = map (package: { inherit package; }) (
       with pkgs.gnomeExtensions;
       [
+        appindicator
         auto-move-windows
         caffeine
         clipboard-indicator
         launch-new-instance
         removable-drive-menu
-        status-icons
         steal-my-focus-window
         system-monitor
         wallpaper-slideshow
