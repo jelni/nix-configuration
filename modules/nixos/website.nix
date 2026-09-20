50cc08f5011d77e29a953c4b6ee9e64c4f5b8810
jel <git@jel.gay>
2026-09-21 00:14:31 +0200

    host my website on clearnet

--- modules/nixos/website.nix
+++ modules/nixos/website.nix
@@ -1,10 +1,27 @@
 { config, inputs, ... }: {
   age.secrets.website-tor-secret-key.file = ../../secrets/website-tor-secret-key.age;
 
-  services = {
-    caddy.virtualHosts."http://zuzia2c2ggz2ljtsdeofhmdviomlljnib5edb5pxmtiffknl3p3xqyyd.onion".extraConfig =
-      ''
+  services =
+    let
+      tor-address = "http://zuzia2c2ggz2ljtsdeofhmdviomlljnib5edb5pxmtiffknl3p3xqyyd.onion";
+    in
+    {
+      caddy.virtualHosts."https://jel.gay, ${tor-address}".extraConfig = ''
+        reverse_proxy /.well-known/matrix/* https://matrix.jel.gay {
+          header_up Host {upstream_hostport}
+        }
+
+        reverse_proxy /.well-known/acme-challenge/* http://prosody.jel.gay {
+          header_up Host {upstream_hostport}
+        }
+
+        reverse_proxy /.well-known/* https://gts.jel.gay {
+          header_up Host {upstream_hostport}
+        }
+
         root ${inputs.website}/public
+        redir /kalkulator /calculator 308
+        header Onion-Location ${tor-address}{uri}
         try_files {path}.html
         file_server
 
@@ -14,9 +31,9 @@
         }
       '';
 
-    tor.relay.onionServices.website = {
-      map = [ 80 ];
-      secretKey = config.age.secrets.website-tor-secret-key.path;
+      tor.relay.onionServices.website = {
+        map = [ 80 ];
+        secretKey = config.age.secrets.website-tor-secret-key.path;
+      };
     };
-  };
 }
